078c7ddab0c27b3c1a2effd7b28499d560951d8d
chelly-hsu <golo126@gmail.com>
2023-05-03 22:54:43 +0800

    test db

--- connections/index.ts
+++ connections/index.ts
@@ -3,9 +3,12 @@ import mongoose from 'mongoose'
 
 dotenv.config({ path: './env/config.env' })
 
-const DB = process.env.DATABASE_URL?.replace('${DATABASE_ACCOUNT}', process.env.DATABASE_ACCOUNT || '')
-  .replace('${DATABASE_PASSWORD}', process.env.DATABASE_PASSWORD || '') 
-  || ''
+const DB = `mongodb+srv://wowcho:1qaz%40WSX@cluster0.fdnrq87.mongodb.net/test`
+
+// const DB = process.env.DATABASE_URL?.replace('${DATABASE_ACCOUNT}', process.env.DATABASE_ACCOUNT || '')
+// .replace('${DATABASE_PASSWORD}', process.env.DATABASE_PASSWORD || '') 
+// || ''
+
 
 mongoose.connect(DB)
   .then(() => console.log('Server 連線成功'))
