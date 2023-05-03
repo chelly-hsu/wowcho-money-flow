62e6e61a33f598e08e7bb4b85369d968e07d8c00
chelly-hsu <golo126@gmail.com>
2023-05-03 23:29:14 +0800

    cancel comment

--- connections/index.ts
+++ connections/index.ts
@@ -6,10 +6,7 @@ dotenv.config({ path: './env/config.env' })
 const DB = process.env.DATABASE_URL?.replace('${DATABASE_ACCOUNT}', process.env.DATABASE_ACCOUNT || '')
   .replace('${DATABASE_PASSWORD}', process.env.DATABASE_PASSWORD || '') 
   || ''
-console.log('process.env.DATABASE_URL',process.env.DATABASE_URL)
-console.log('${DATABASE_ACCOUNT}', process.env.DATABASE_ACCOUNT)
-console.log('${DATABASE_PASSWORD}', process.env.DATABASE_PASSWORD)
-console.log('DB',DB)
+
 mongoose.connect(DB)
   .then(() => console.log('Server 連線成功'))
   .catch(e => console.log(e))
