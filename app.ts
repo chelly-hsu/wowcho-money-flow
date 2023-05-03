3eab15b0e9c58a2bf33d00ee2ec3284ff83a4f16
chelly-hsu <golo126@gmail.com>
2023-05-03 23:36:33 +0800

    open uploadRouter

--- app.ts
+++ app.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 import { userRouter } from './routes/User.route'
 import { moneyFlowRouter } from './routes/MoneyFlow.route'
-// import { uploadRouter } from './routes/Upload.route'
+import { uploadRouter } from './routes/Upload.route'
 import { promisesRouter } from './routes/Promises.route'
 
 const app = express()
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 // TODO 先放根目錄提供會員功能還沒好前串接測試，有會員後需要移到各自 API 內
-// app.use('/upload', uploadRouter)
+app.use('/upload', uploadRouter)
 
 app.use('/profile', userRouter)
 app.use('/proposal/promises', promisesRouter)
