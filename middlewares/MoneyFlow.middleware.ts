a1bed685e122fac518ee3d4791b5e1623c31fd59
chelly-hsu <111692708+chelly-hsu@users.noreply.github.com>
2023-05-03 16:13:31 +0800

    format

--- middlewares/MoneyFlow.middleware.ts
+++ middlewares/MoneyFlow.middleware.ts
@@ -1,7 +1,7 @@
 // import handleErrorAsync from "../services/handleErrorAsync"
 // import { appError } from '../services/appError'
 // import { Register } from '../models/Register.model'
-import * as crypto from 'crypto'
+import crypto from "crypto"
 
 export const { MerchantID, HASHKEY, HASHIV, Version, Host } = process.env
 export const RespondType = 'JSON'
@@ -49,7 +49,7 @@ export function create_mpg_aes_decrypt(TradeInfo) {
   return JSON.parse(result);
 }
 
-module.exports = {
+export default {
   create_mpg_aes_encrypt,
   create_mpg_sha_encrypt,
   create_mpg_aes_decrypt
